f1bc8a6a1d3dc7f06a82599579c5e6d02d2fa56d
TechnicFreakz <luki.baldner@gmail.com>
2023-12-06 15:41:06 +0100

    fix import error

--- olymp_bot.py
+++ olymp_bot.py
@@ -2,7 +2,6 @@ import discord
 import os
 from dotenv import load_dotenv
 import random
-from .constants import *
 
 load_dotenv()
 bot = discord.Bot()
@@ -13,6 +12,7 @@ async def on_ready():
     print(f"{bot.user} is ready and online!")
 
 
+from .constants import hello_response
 @bot.slash_command(name = "hello", description = "Say hello to the bot")
 async def hello(ctx):
     await ctx.respond(hello_response)
@@ -21,10 +21,12 @@ async def hello(ctx):
 async def answer(ctx):
     await ctx.respond("42")
 
+from .constants import wrong_citations
 @bot.slash_command(name = "fzitat", description = "Ein zufälliges falsch zugeordnetes Zitat bitte!")
 async def fzitat(ctx):
     await ctx.respond(wrong_citations[random.randint(0, len(wrong_citations) - 1)])
-    
+
+from .constants import soon_response
 @bot.slash_command(name = "soon", description = "When will it happen?")
 async def hello(ctx):
     await ctx.respond(soon_response)
