08ac0f250b2c79f75fa99b37428f82afc1c79a4a
TechnicFreakz <luki.baldner@gmail.com>
2023-12-06 18:10:10 +0100

    Add greetings for server join

--- olymp_bot.py
+++ olymp_bot.py
@@ -11,10 +11,16 @@ bot = discord.Bot()
 
 api = fortnite_api.FortniteAPI(os.getenv('FN_STATS_API_TOKEN'))
 
+intents = discord.Intents()
+intents.members = True
+
 @bot.event
 async def on_ready():
     print(f"{bot.user} is ready and online!")
 
+@bot.event
+async def on_member_join(member):
+    await member.send(f"Welcome to the server '{member.guild.name}'")
 
 @bot.slash_command(name = "hello", description = "Say hello to the bot")
 async def hello(ctx):
