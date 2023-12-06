cb30147e56de0fe1d642e6763e5b25dfdcc6034f
Lukas Baldner <lukas@baldner.org>
2023-12-06 13:43:57 +0000

    readd old commands

--- olymp_bot.py
+++ olymp_bot.py
@@ -1,6 +1,7 @@
 import discord
 import os
 from dotenv import load_dotenv
+import random
 
 load_dotenv()
 bot = discord.Bot()
@@ -16,6 +17,7 @@ wrong_citations = ['„Frage nicht, was dein Land für dich tun kann, frage was 
 async def on_ready():
     print(f"{bot.user} is ready and online!")
 
+
 @bot.slash_command(name = "hello", description = "Say hello to the bot")
 async def hello(ctx):
     await ctx.respond("Hey!")
@@ -23,5 +25,9 @@ async def hello(ctx):
 @bot.slash_command(name = "answer", description = "What do you get if you multiply six by nine?")
 async def answer(ctx):
     await ctx.respond("42")
+
+@bot.slash_command(name = "fzitat", description = "Ein zufälliges falsch zugeordnetes Zitat bitte!")
+async def fzitat(ctx):
+    await ctx.respond(wrong_citations[random.randint(0, 3)])
 
 bot.run(os.getenv('OLYMP_TOKEN'))
